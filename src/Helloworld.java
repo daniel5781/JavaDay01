04f84d02db8590b3f2effe1842ce729b89f27b6f
user <user@com11>
2017-10-24 20:59:59 +0900

    mysecond edit for java

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -2,8 +2,15 @@
 public class Helloworld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+		System.out.println("Hello World!!");
+		System.out.println("Hello World!!");
+		System.out.println("Hello World!!");
+		System.out.println("Hello World!!");
+		System.out.println("Hello World!!");
+		System.out.println("Hello World!!");
 
+		
 	}
 
 }
